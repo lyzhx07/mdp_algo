d707a5551aeb5209533a964201df10e3908848ae
zhx <zhanghx96@gmail.com>
2019-02-08 02:14:46 +0800

    MapConstants class
    Define some constants for the map

---
+++ src/Map/MapConstants.java
@@ -0,0 +1,14 @@
+package Map;
+
+public class MapConstants {
+
+    // Public Map Variables
+    public static final short CELL_CM = 10;
+    public static final short MAP_HEIGHT = 20;
+    public static final short MAP_WIDTH = 15;
+    public static final short GOALZONE_ROW = 18;
+    public static final short GOALZONE_COL = 13;
+    public static final short STARTZONE_ROW = 1;
+    public static final short STARTZONE_COL = 1;
+
+}
